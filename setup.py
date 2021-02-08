6447b2b8ae3b3d078c9af8ba69e1f50cb999108c
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2021-02-07 21:36:08 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="discord-menu",
-    version="0.12.0",
+    version="0.13.0",
     author="The Tsubotki Team",
     author_email="69992611+TsubakiBotPad@users.noreply.github.com",
     license="Apache-2.0 License",
