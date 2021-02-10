e482aabbf69f044718177371e4643baac6d78742
River <18037011+RheingoldRiver@users.noreply.github.com>
2021-02-10 04:26:28 -0600

    Support None returned from embed_control functions (#24)
    * UnsupportedPaneTypes to show disallowed actions
    
    * Bump version
    
    * Return None instead of raising an error
    
    * Return None instead of error & action -> transition

--- discordmenu/embed/emoji.py
+++ discordmenu/embed/emoji.py
@@ -1,9 +1,10 @@
 class EmbedMenuEmojiConfig:
-    def __init__(self, delete_message="❌"):
+    def __init__(self, delete_message="❌", unsupported_transition="🚫"):
         self.delete_message = delete_message
+        self.unsupported_transition = unsupported_transition
 
     def to_list(self):
-        return [self.delete_message]
+        return [self.delete_message, self.unsupported_transition]
 
 
 DEFAULT_EMBED_MENU_EMOJI_CONFIG = EmbedMenuEmojiConfig()
--- discordmenu/embed/menu.py
+++ discordmenu/embed/menu.py
@@ -1,7 +1,7 @@
+import asyncio
 from typing import Callable, List, Optional, Coroutine, Dict
 
 from discord import Message, RawReactionActionEvent
-
 from discordmenu.discord_client import remove_reaction, update_embed_control, send_embed_control, \
     diff_emojis_raw
 from discordmenu.embed.control import EmbedControl
@@ -20,11 +20,14 @@ class EmbedMenu:
                  reaction_filters: List[ReactionFilter],
                  transitions: Dict[str, NextEmbedControlFunc],
                  initial_pane: Callable,
-                 emoji_config: EmbedMenuEmojiConfig = DEFAULT_EMBED_MENU_EMOJI_CONFIG):
+                 emoji_config: EmbedMenuEmojiConfig = DEFAULT_EMBED_MENU_EMOJI_CONFIG,
+                 unsupported_transition_announce_timeout: int = 3
+                 ):
         self.emoji_config = emoji_config
         self.transitions = transitions
         self.reaction_filters = reaction_filters
         self.initial_pane = initial_pane
+        self.unsupported_transition_announce_timeout = unsupported_transition_announce_timeout
 
     async def create(self, ctx, state: ViewState):
         embed_control: EmbedControl = self.initial_pane(state)
@@ -43,15 +46,23 @@ class EmbedMenu:
             return
 
         new_control = await transition_func(message, ims, **data)
+        if new_control is not None:
+            current_emojis = [e.emoji for e in message.reactions]
+            next_emojis = new_control.emoji_buttons + [emoji_cache.get_emoji(self.emoji_config.delete_message)]
 
-        current_emojis = [e.emoji for e in message.reactions]
-        next_emojis = new_control.emoji_buttons + [emoji_cache.get_emoji(self.emoji_config.delete_message)]
-
-        emoji_diff = diff_emojis_raw(current_emojis, next_emojis)
-        await update_embed_control(message, new_control, emoji_diff)
+            emoji_diff = diff_emojis_raw(current_emojis, next_emojis)
+            await update_embed_control(message, new_control, emoji_diff)
+        elif self.emoji_config.unsupported_transition is not None:
+            # allow the reporting of an unsupported transition to be nulled by config
+            await message.add_reaction(self.emoji_config.unsupported_transition)
+            asyncio.create_task(self.remove_unsupported_action_response(message))
 
         if message.guild:
             await remove_reaction(message, emoji_clicked, member.id)
+
+    async def remove_unsupported_action_response(self, message):
+        await asyncio.sleep(self.unsupported_transition_announce_timeout)
+        await message.clear_reaction(self.emoji_config.unsupported_transition)
 
     async def should_respond_raw(self, message, event: RawReactionActionEvent):
         for reaction_filter in self.reaction_filters:
