da12ebf69621285be550b097a23a06e1d33a02ee
River <18037011+RheingoldRiver@users.noreply.github.com>
2021-05-19 01:33:17 -0500

    Add Box to EmbedField title type checking (#33)

--- discordmenu/embed/components.py
+++ discordmenu/embed/components.py
@@ -11,7 +11,7 @@ class EmbedField(CustomMapping):
     def fields(self) -> List[str]:
         return ["name", "value", "inline"]
 
-    def __init__(self, title: str, body: Union[Box, str], inline: bool = False, chunk_delimiter='\n',
+    def __init__(self, title: Union[Box, str], body: Union[Box, str], inline: bool = False, chunk_delimiter='\n',
                  continuation_title=''):
         self.continuation_title = continuation_title
         self.chunk_delimiter = chunk_delimiter
