8bea857a847a93bec2f9ee0965709f1ed5eccc17
AA <chasehult@gmail.com>
2021-08-05 00:13:21 -0700

    Don't delete Box contents on init

--- discordmenu/embed/base.py
+++ discordmenu/embed/base.py
@@ -3,20 +3,20 @@ from typing import List, Mapping, Union, Iterable, Any
 
 
 class Box:
-    def __init__(self, *args: Union[str, "Box"], delimiter: str = "\n"):
-        self._inner_object = [a for a in args if a]
+    def __init__(self, *contents: Union[str, "Box"], delimiter: str = "\n"):
+        self.contents = contents
         self.delimiter = delimiter
 
     def _get_markdown(self, arg: Union[str, "Box"]) -> str:
-        if hasattr(arg, 'to_markdown'):
+        if isinstance(arg, Box):
             return arg.to_markdown()
         return arg
 
     def to_markdown(self) -> str:
-        return self.delimiter.join(self._get_markdown(a) for a in self._inner_object)
+        return self.delimiter.join(self._get_markdown(a) for a in self.contents if a)
 
     def __bool__(self) -> bool:
-        return any(self._inner_object)
+        return any(self.contents)
 
 
 class CustomMapping(Mapping[str, Any], ABC):
@@ -24,8 +24,7 @@ class CustomMapping(Mapping[str, Any], ABC):
         return len(self.fields)
 
     def __iter__(self) -> Iterable[str]:
-        for f in self.fields:
-            yield f
+        yield from self.fields
 
     def __getitem__(self, key: str) -> Any:
         ret = getattr(self, key)
